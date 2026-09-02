93dea9cee704c78f63574d1a973ea7d6f6bae18d
erfaansiddikadeeb-creator <erfaansiddikadeeb@gmail.com>
2026-09-02 10:53:44 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import ResumeTailor from "./pages/ResumeTailor.jsx";
+import ContractChecker from "./pages/ContractChecker.jsx";
+import Privacy from "./pages/Privacy.jsx";
+import NotFound from "./pages/NotFound.jsx";
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/resume-tailor" element={<ResumeTailor />} />
+      <Route path="/contract-checker" element={<ContractChecker />} />
+      <Route path="/privacy" element={<Privacy />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+}
